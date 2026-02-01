e3ed2d9b164cf65866c71ea1cee2fc0e5ca5ba19
Laiba Nadeem <miss.laiba.nadeem@gmail.com>
2026-02-01 16:14:30 +0500

    Update import paths for langchain modules

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 # --- 3. PAGE CONFIG ---
